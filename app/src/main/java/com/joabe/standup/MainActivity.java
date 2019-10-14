696ada6d6cdfe7bb8e7257d59ec26c26ada46199
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-14 13:31:35 -0300

    Setting up the notification

--- app/src/main/java/com/joabe/standup/MainActivity.java
+++ app/src/main/java/com/joabe/standup/MainActivity.java
@@ -1,7 +1,15 @@
 package com.joabe.standup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
@@ -9,6 +17,11 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+    private NotificationManager mNotificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +35,54 @@ public class MainActivity extends AppCompatActivity {
                 String toastMessage;
 
                 if (isChecked) {
+                    deliverNotification(MainActivity.this);
                     toastMessage = getString(R.string.stand_up_alarm_on);
                 } else {
+                    mNotificationManager.cancelAll();
                     toastMessage = getString(R.string.stand_up_alarm_off);
                 }
 
                 Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
             }
         });
+
+        createNotificationChannel();
+
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    public void createNotificationChannel() {
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    PRIMARY_CHANNEL_ID,
+                    getString(R.string.stand_up_notification),
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notifies every 15 minutes to stand up and walk");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void deliverNotification(Context context) {
+        Intent contentIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity(
+                context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,
+                PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stand_up)
+                .setContentTitle(getString(R.string.stand_up_alert))
+                .setContentText(getString(R.string.stand_up_meessage))
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
