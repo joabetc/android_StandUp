07a7ccec662b6f1236b7444efca0d575168fa3b3
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-14 11:20:25 -0300

    Setting up the setOnCheckedChangeListener() method

--- app/src/main/java/com/joabe/standup/MainActivity.java
+++ app/src/main/java/com/joabe/standup/MainActivity.java
@@ -3,6 +3,10 @@ package com.joabe.standup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
+        alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                String toastMessage;
+
+                if (isChecked) {
+                    toastMessage = getString(R.string.stand_up_alarm_on);
+                } else {
+                    toastMessage = getString(R.string.stand_up_alarm_off);
+                }
+
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
